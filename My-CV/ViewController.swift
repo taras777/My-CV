4a6380c81ce39bd656f7e8b9cd8ad25a1df4f968
taras777 <taras.kuzmenko777@gmail.com>
2016-05-08 23:21:10 +0300

    added files for My-CV

--- My-CV/ViewController.swift
+++ My-CV/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hobbiesBtn: UIButton!
+    @IBOutlet weak var programmingBtn: UIButton!
+    @IBOutlet weak var aboutMeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        aboutMeBtn.layer.cornerRadius = 2.0
+        programmingBtn.layer.cornerRadius = 2.0 
+        hobbiesBtn.layer.cornerRadius = 2.0
     }
 
     override func didReceiveMemoryWarning() {
